c7d0ff1d920a828402fb7b17e67b0b76bbae6b58
kghafari <kghafari@users.noreply.github.com>
2020-02-25 22:00:34 -0500

    push

---
+++ Day27-StateManagement_ViewBags_ViewData_TempData/Controllers/TempDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day27_StateManagement_ViewBags_ViewData_TempData.Controllers
+{
+    public class TempDataController : Controller
+    {
+       
+        public IActionResult First()
+        {
+            //setting our temp data
+            //this populates the TempData[""]
+
+            //        key        value
+            TempData["Jon"] = "JonsCars";
+            return View();
+        }
+
+        public IActionResult Second()
+        {
+            //set a variable to our temp data
+            // ?? null : sets it to either JonsCars, or null (so we don't get an error)
+            var jon = TempData["JonsCars"] ?? null;
+            return View();
+        }
+
+        public IActionResult Third()
+        {
+            var jon = TempData["JonsCars"] ?? null;
+            return View();
+        }
+
+        public IActionResult Fourth()
+        {
+            var jon = TempData["JonsCars"] ?? null;
+            return View();
+        }
+    }
+}
---
+++ Day27-StateManagement_ViewBags_ViewData_TempData/Controllers/ViewBagViewDataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day27_StateManagement_ViewBags_ViewData_TempData.Controllers
+{
+    public class ViewBagViewDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            //creating viewbags:
+            ViewBag.Cats = "Spaghetti";
+            ViewBag.Dogs = "Mojo";
+
+            //ViewBags & ViewData with the same key(name) point to the same piece of memory.
+            ViewData["Cats"] = "Toad"; //points to the EXACT same piece of memory as ViewBag.Cats
+
+            //HTML will print "Toat, Mojo, Toad"
+            return View();
+        }
+
+        public IActionResult ListCatsWithViewBag()
+        {
+            //"gross"
+            ViewBag.Cats = new List<string> 
+            {
+                "Spaghetti", "Mojo", "Sprinkles"
+            };
+            return View();
+        }
+    }
+}
